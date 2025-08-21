6c565b09d3070e5419f4fac69f10ca0304e43a44
xabdulla <abdullashaker002@gmail.com>
2025-08-21 15:41:08 +0300

    added admin and cat detail with css

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('cats/', views.cat_index, name='cat-index'),
-    
+    path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,35 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Cat
 
-
-#Temp cat 
-
-class Cat:
-
-    def __init__(self, name, age, breed, description):
-        self.name = name
-        self.breed = breed
-        self.age = age
-        self.description = description
-
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]        
-
-
-# Create your views here.
 def home(request):
-    return render(request, 'home.html')
-
-# main_app/views.py
+  return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
-
-# views.py
+  return render(request, 'about.html')
 
 def cat_index(request):
-    return render(request, 'cats/index.html', {'cats': cats})
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', { 'cats': cats })
+  
+def cat_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  print('cat: ', cat)
+  return render(request, 'cats/detail.html', { 'cat': cat })
